822b615dde01f55f53e0e3dc7cc4bbf0f7e8159a
Rohit Verma <rohit.verma@niki.ai>
2019-12-13 22:10:23 +0530

    bumped mq version and postgres version to support aws rds and amazon mq

--- service/src/main/java/org/apache/fineract/cn/provisioner/config/ProvisionerServiceConfig.java
+++ service/src/main/java/org/apache/fineract/cn/provisioner/config/ProvisionerServiceConfig.java
@@ -98,8 +98,9 @@ public class ProvisionerServiceConfig extends WebMvcConfigurerAdapter {
     final PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
     final ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
     activeMQConnectionFactory.setBrokerURL(activeMQProperties.getBrokerUrl());
+    activeMQConnectionFactory.setUserName("fineract");
+    activeMQConnectionFactory.setPassword("fineract1234");
     pooledConnectionFactory.setConnectionFactory(activeMQConnectionFactory);
-
     return pooledConnectionFactory;
   }
 
